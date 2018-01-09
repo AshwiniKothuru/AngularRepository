3e920d060fd0bb4307c566d9ee8eeadd533bbf43
Kothuru <Ashwini.Kothuru@thomsonreuters.com>
2018-01-09 19:43:02 +0530

    files added

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+ text1:string='Yipeee!!';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
